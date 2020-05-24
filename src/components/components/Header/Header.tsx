58fbe37ecc9d703e172244f89d4cd785709a7980
AdilBikeev <bikeev1999@mail.ru>
2020-05-24 11:50:54 +0300

    foo-branch Переписал дизайн Header под Material UI

--- src/components/components/Header/Header.tsx
+++ src/components/components/Header/Header.tsx
@@ -1,13 +1,47 @@
 ﻿import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Container } from '@material-ui/core';
+import { Toolbar, Typography, IconButton, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, Hidden } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 0
-  },
-});
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      backgroundColor: "#EF6767",
+    },
+    hide: {
+      display: 'none',
+    },
+    tabItems: {
+      flexGrow: 1,
+      textAlign: "center"
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      backgroundColor: "#494949",
+      color: "white"
+    },
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-start',
+    },
+  }),
+);
 
 type Props = {
   title: string;
@@ -17,14 +51,79 @@ type Props = {
 export const Header: React.FC<Props> = ({
   title,
   courses
-  }) => {
+}) => {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   return (
-    <Container className={classes.root}>
-      {courses.map(x => (
-        <Button onClick={() => { }}>{x}</Button>
-      ))}
-    </Container>
+    <div>
+
+    <Toolbar className={classes.root}>
+
+      {/* Title */}
+      <Typography variant="h6" noWrap>
+        StudyNow
+      </Typography>
+
+
+
+      {/* Вкладки */}
+      <div className={classes.tabItems}>
+        <Hidden xsDown>
+          {courses.map(x => (
+            <Button key={x} onClick={() => { }}>{x}</Button>
+          ))}
+        </Hidden>
+      </div>
+
+      {/* Кнопка выпадающего меню */}
+      <div>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="end"
+          onClick={toggleDrawer}
+          className={clsx(open && classes.hide)}
+        >
+          <MenuIcon />
+        </IconButton>
+      </div>
+    </Toolbar>
+
+      {/* Выпадающее меню */}
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}>
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={toggleDrawer}>
+            {theme.direction === 'rtl' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+
+        <List>
+          {courses.map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                <MenuIcon />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+
+      </Drawer>
+
+    </div>
   );
 }
