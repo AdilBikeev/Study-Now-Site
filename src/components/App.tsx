467519b4be8bc1e79c4aebf4663f52d063d2420e
AdilBikeev <bikeev1999@mail.ru>
2020-06-21 14:24:07 +0300

    WEB-1 Исправил логику работы роутинга

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import { HomeContainer } from './components/Content/Home/HomeContainer';
 import { FooterContainer } from './components/Footer/FooterContainer';
 import { CourseContainer } from './components/Content/Course/CourseContainer';
@@ -7,12 +7,12 @@ import { CourseContainer } from './components/Content/Course/CourseContainer';
 export const App = () => {
   return (
     <BrowserRouter>
-
+      <Switch>
         <Route exact path='/Home' render={() => <HomeContainer />} />
         <Route exact path='/Course/:nameCourse?' render={() => <CourseContainer />} />
         <Route path='*'
           render={() => <div>404 NOT FOUND</div>} />
-
+      </Switch>
       <FooterContainer />
     </BrowserRouter>
   );
